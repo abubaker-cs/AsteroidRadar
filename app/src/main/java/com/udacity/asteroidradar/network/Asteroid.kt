e0cc3e7ad29ee091bdd0395af30bb96f3083a4ef
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-01 03:50:28 +0500

    Working on the Entities

--- app/src/main/java/com/udacity/asteroidradar/network/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/Asteroid.kt
@@ -1,12 +1,50 @@
 package com.udacity.asteroidradar.network
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+/**
+ * Asteroid entity to be stored in the table_asteroid.
+ */
+
+@Entity(tableName = "table_asteroid")
 @Parcelize
 data class Asteroid(
-    val id: Long, val codename: String, val closeApproachDate: String,
-    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-    val relativeVelocity: Double, val distanceFromEarth: Double,
+
+    // ID
+    @PrimaryKey
+    @ColumnInfo(name = "asteroid_id")
+    val id: Long,
+
+    // Code Name
+    @ColumnInfo(name = "code_name")
+    val codename: String,
+
+    // Close Approach Date
+    @ColumnInfo(name = "close_approach_date")
+    val closeApproachDate: String,
+
+    // Absolute Magnitude
+    @ColumnInfo(name = "absolute_magnitude")
+    val absoluteMagnitude: Double,
+
+    // Estimated Diameter
+    @ColumnInfo(name = "estimate_diameter")
+    val estimatedDiameter: Double,
+
+    // Relative Velocity
+    @ColumnInfo(name = "relative_velocity")
+    val relativeVelocity: Double,
+
+    // Distance from Earth
+    @ColumnInfo(name = "distance_from_earth")
+    val distanceFromEarth: Double,
+
+    // Hazardous?
+    @ColumnInfo(name = "danger_status")
     val isPotentiallyHazardous: Boolean
+
 ) : Parcelable
--- app/src/main/java/com/udacity/asteroidradar/network/PictureOfDay.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/PictureOfDay.kt
@@ -1,9 +1,25 @@
 package com.udacity.asteroidradar.network
 
-import com.squareup.moshi.Json
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+/**
+ * PictureOfDay entity to be stored in the table_pic_of_day.
+ */
+@Entity(tableName = "table_pic_of_day")
 data class PictureOfDay(
-    @Json(name = "media_type") val mediaType: String,
+
+    // Media Type
+    @ColumnInfo(name = "media_type")
+    val mediaType: String,
+
+    // Title
+    @ColumnInfo(name = "title")
     val title: String,
+
+    // URL
+    @PrimaryKey
     val url: String
+
 )
