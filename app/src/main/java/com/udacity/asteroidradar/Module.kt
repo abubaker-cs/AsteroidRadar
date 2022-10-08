4c2cc34ff54b865fc4fbfb9d6ef5464d0bc7ed9d
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-09 03:19:07 +0500

    Fixing bug related to loading data in RecyclerView

--- app/src/main/java/com/udacity/asteroidradar/Module.kt
+++ app/src/main/java/com/udacity/asteroidradar/Module.kt
@@ -3,5 +3,9 @@ package com.udacity.asteroidradar
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
+/**
+ * This class is being used to avoid Glide's bug: Failed to find GeneratedAppGlideModule...
+ * Solution: https://github.com/bumptech/glide/issues/4476#issuecomment-1113213037
+ */
 @GlideModule
 class GlideAppModule : AppGlideModule()
