2d36fadb75e04108008f65c92ccc98ed7543d7df
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-10 04:55:10 +0500

    Moshi does not seems to be compatible with the API, replaced it with gson

--- app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -11,3 +11,11 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
+/**
+ * TODO 01 - App Icon
+ * TODO 02 - Add comments throughout the project
+ * TODO 03 - Screenshots
+ * TODO 04 - Update README.md
+ * TODO 05 - replace depreciated onCreateOptionsMenu & onOptionsItemSelected
+ */
