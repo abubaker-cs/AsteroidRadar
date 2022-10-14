4de0b008ac8671506f2f689ff42ed7e63b99c314
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-15 02:27:31 +0500

    Replaced YYYY-MM-dd with MM/dd/yyyy in the Constants.kt

--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -10,11 +10,17 @@ object Constants {
     //
     const val API_KEY = BuildConfig.nasaApiKey
 
-    // Issue:
-    // YYYY-MM-dd causes @SuppressLint("WeekBasedYear") ERROR, so I am replacing it with MM/dd/yyyy
-    //
-    // Week Based Year:
-    // https://googlesamples.github.io/android-custom-lint-rules/checks/WeekBasedYear.md.html
+    /**
+     * Issue: Week Based Year
+     * The DateTimeFormatter pattern YYYY returns the week based year, not the era-based year.
+     * This means that 12/29/2019 will format to 2019, but 12/30/2019 will format to 2020!
+     *
+     * Reference:
+     * https://googlesamples.github.io/android-custom-lint-rules/checks/WeekBasedYear.md.html
+     *
+     * Solution:
+     * YYYY-MM-dd causes @SuppressLint("WeekBasedYear") ERROR, so I am replacing it with MM/dd/yyyy
+     */
     const val API_QUERY_DATE_FORMAT = "MM/dd/yyyy"
 
     //
