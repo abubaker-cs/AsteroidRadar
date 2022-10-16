357bf87a17093687d708149534868fd9b6e2b770
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-16 05:11:31 +0500

    Working on displaying total records using Toast Message

--- app/src/main/java/com/udacity/asteroidradar/main/AsteroidsAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidsAdapter.kt
@@ -7,7 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.data.model.Asteroid
 import com.udacity.asteroidradar.databinding.ListItemAsteroidBinding
 
-class AsteroidsAdapter(private val clickListener: (asteroid: Asteroid) -> Unit) :
+
+class AsteroidsAdapter(private val onClickListener: (asteroid: Asteroid) -> Unit) :
     RecyclerView.Adapter<AsteroidsAdapter.AsteroidViewHolder>() {
 
     private lateinit var asteroids: List<Asteroid>
@@ -19,11 +20,25 @@ class AsteroidsAdapter(private val clickListener: (asteroid: Asteroid) -> Unit) 
         notifyDataSetChanged()
     }
 
+    /**
+     * Allows the RecyclerView to determine which items have changed when the [List] of [Asteroid]
+     * has been updated.
+     */
+//    companion object DiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+//        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+//            return oldItem === newItem
+//        }
+//
+//        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+//            return oldItem.id == newItem.id
+//        }
+//    }
+
     // Bind data using the list_item_asteroid.xml file
     inner class AsteroidViewHolder(private val binding: ListItemAsteroidBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bindAsteroid(clickListener: (asteroid: Asteroid) -> Unit, asteroid: Asteroid) {
+        fun bind(clickListener: (asteroid: Asteroid) -> Unit, asteroid: Asteroid) {
 
             //
             binding.asteroid = asteroid
@@ -33,63 +48,28 @@ class AsteroidsAdapter(private val clickListener: (asteroid: Asteroid) -> Unit) 
                 clickListener(asteroid)
             }
 
-            //
+            // This is important, because it forces the data binding to execute immediately,
+            // which allows the RecyclerView to make the correct view size measurements
             binding.executePendingBindings()
         }
 
     }
 
     /**
-     * Called when RecyclerView needs a new [AsteroidViewHolder] of the given type to represent
-     * an item.
-     *
-     *
-     * This new AsteroidViewHolder should be constructed with a new View that can represent the items
-     * of the given type. You can either create a new View manually or inflate it from an XML
-     * layout file.
-     *
-     *
-     * The new AsteroidViewHolder will be used to display items of the adapter using
-     * [.onBindViewHolder]. Since it will be re-used to display
-     * different items in the data set, it is a good idea to cache references to sub views of
-     * the View to avoid unnecessary [View.findViewById] calls.
-     *
-     * @param parent   The ViewGroup into which the new View will be added after it is bound to
-     * an adapter position.
-     * @param viewType The view type of the new View.
-     * @return A new AsteroidViewHolder that holds a View of the given view type.
-     * @see .getItemViewType
-     * @see .onBindViewHolder
+     * Create new [RecyclerView] item views (invoked by the layout manager)
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidViewHolder {
 
         binding =
             ListItemAsteroidBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
         return AsteroidViewHolder(binding)
 
     }
 
 
     /**
-     * Called by RecyclerView to display the data at the specified position. This method should
-     * update the contents of the [AsteroidViewHolder.itemView] to reflect the item at the given
-     * position.
-     *
-     *
-     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
-     * again if the position of the item changes in the data set unless the item itself is
-     * invalidated or the new position cannot be determined. For this reason, you should only
-     * use the `position` parameter while acquiring the related data item inside
-     * this method and should not keep a copy of it. If you need the position of an item later
-     * on (e.g. in a click listener), use [AsteroidViewHolder.getBindingAdapterPosition] which
-     * will have the updated adapter position.
-     *
-     * Override [.onBindViewHolder] instead if Adapter can
-     * handle efficient partial bind.
-     *
-     * @param holder   The AsteroidViewHolder which should be updated to represent the contents of the
-     * item at the given position in the data set.
-     * @param position The position of the item within the adapter's data set.
+     * Replaces the contents of a view (invoked by the layout manager)
      */
     override fun onBindViewHolder(holder: AsteroidViewHolder, position: Int) {
 
@@ -97,13 +77,9 @@ class AsteroidsAdapter(private val clickListener: (asteroid: Asteroid) -> Unit) 
         val asteroid = asteroids[position]
 
         //
-        holder.bindAsteroid(clickListener, asteroid)
+        holder.bind(onClickListener, asteroid)
 
     }
-
-//    class OnClickListener(val clickListener: (asteroid: Asteroid) -> Unit) {
-//        fun onClick(asteroid: Asteroid) = clickListener(asteroid)
-//    }
 
     /**
      * Returns the total number of items in the data set held by the adapter.
