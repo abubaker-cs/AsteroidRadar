5039d76fc276015f12fc1699fc78c66dbacac79b
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-19 00:07:30 +0500

    Commented: dailyRecords.kt and weeklyRecords.kt

--- app/src/main/java/com/udacity/asteroidradar/data/weeklyRecords.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/weeklyRecords.kt
@@ -8,19 +8,42 @@ import java.util.*
 @SuppressLint("WeekBasedYear")
 fun weeklyRecords(): String {
 
+    // This method is used with calendar object to get the instance of calendar according to current time zone
+    // =======================================================================================================
+    // It will return a calendar of a type appropriate to the locale, whose time fields have been initialized
+    // with the current date and time
     //
-    val calendar = Calendar.getInstance()
+    // Reference: https://developer.android.com/reference/kotlin/android/icu/util/Calendar
+    val rightNow = Calendar.getInstance()
 
-    //
-    calendar.add(Calendar.DAY_OF_YEAR, 7)
+    // We are assigning a parameter and asking the system to provide us Weekly Calendar (7-Days)
+    rightNow.add(Calendar.DAY_OF_YEAR, 7)
 
-    //
-    val currentTime = calendar.time
+    // Get the Current Time
+    val currentTime = rightNow.time
 
-    //
-    val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+    // Selected Pattern: YYYY-MM-dd (Defined in Constants.kt)
+    // ======================================================
+    // SimpleDateFormat() is used for formatting and parsing dates in a locale-sensitive manner.
+    // It allows for:
+    // 1. Formatting (date → text),
+    // 2. Parsing (text → date), and
+    // 3. Normalization
+    val dateFormattingPattern =
+        SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
 
-    //
-    return dateFormat.format(currentTime)
+    // Apply the selected pattern (YYYY-MM-dd) to the current date/time
+    return dateFormattingPattern.format(currentTime)
 
 }
+
+/**
+ * Linting Issue
+ * =============
+ * Week Based Year The DateTimeFormatter pattern YYYY returns the week based year, not the era-based year.
+ * This means that 12/29/2019 will format to 2019, but 12/30/2019 will format to 2020!
+ *
+ * Solution: YYYY-MM-dd causes @SuppressLint("WeekBasedYear") ERROR, so I am replacing it with MM/dd/yyyy
+ *
+ * Reference: https://googlesamples.github.io/android-custom-lint-rules/checks/WeekBasedYear.md.html
+ */
