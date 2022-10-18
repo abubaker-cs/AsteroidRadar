cef34704fd4fa1a255b7349e90f868edc433a62c
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-18 23:14:20 +0500

    Commented: AsteroidDao.kt

--- app/src/main/java/com/udacity/asteroidradar/data/database/dao/AsteroidDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/database/dao/AsteroidDao.kt
@@ -14,19 +14,19 @@ import com.udacity.asteroidradar.data.model.Asteroid
 @Dao
 interface AsteroidDao {
 
-    // Insert an asteroid's record into the database by using the OnConflictStrategy.REPLACE
+    // I am inserting an asteroid's record into the database using the OnConflictStrategy.REPLACE strategy
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(asteroid: List<Asteroid>)
 
-    // Asteroids: Week
+    // List of Weekly Asteroids
     @Query("SELECT * FROM table_asteroid WHERE close_approach_date >= :startDay AND close_approach_date <= :endDay ORDER BY close_approach_date")
     suspend fun getAsteroidsFromThisWeek(startDay: String, endDay: String): List<Asteroid>
 
-    // Asteroids: Today
+    // List of Today's Asteroids
     @Query("SELECT * FROM table_asteroid WHERE close_approach_date = :today ")
     suspend fun getAsteroidToday(today: String): List<Asteroid>
 
-    // Asteroids: All saved records
+    // List of all saved Asteroids
     @Query("SELECT * from table_asteroid")
     suspend fun getAsteroids(): List<Asteroid>
 
